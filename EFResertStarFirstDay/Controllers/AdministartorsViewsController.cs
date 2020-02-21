16187efb0741ec3bbe03877a3246ce16e49f4c21
伟 黄 <1316279031@qq.com>
2020-02-21 15:57:06 +0800

    AddHtmlViewPageIsLayOutForAdministrator

---
+++ EFResertStarFirstDay/Controllers/AdministartorsViewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFResertStarFirstDay.Controllers
+{
+    public class AdministartorsViewsController : Controller
+    {
+        // GET: AdministartorsViews
+        public ActionResult XzViews()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult XzViews(string [] args)
+        {
+            return View();
+        }
+    }
+}
